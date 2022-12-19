9db54f17d83a6bff747cd2f6f13d5837129d00c2
David <david@gapsandposts.com>
2022-12-19 08:11:16 +0000

    Comiler written but not taking output arg.

--- compiler.py
+++ compiler.py
@@ -1,11 +1,18 @@
 from argparse import ArgumentParser
+from pdflatex import PDFLaTeX
+from base64 import b64decode
 
 TEX_FILE = 'basic.tex'
+
+ENV = {"-output-directory": "./compiled/"}
 
 def compile(output_dir):
     """
     Function to compile the LaTex file into a PDF
     """
+    
+    PDFLaTeX.from_texfile(TEX_FILE).create_pdf(keep_pdf_file=True)
+    
     return
 
 if __name__ == "__main__":
