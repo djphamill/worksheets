f23140a357e824f673a8031310f2d770b55ee559
David <david@gapsandposts.com>
2022-12-24 09:06:40 +0000

    Removed old test files. Change args to take whole path, including file name.

--- generateworksheet.py
+++ generateworksheet.py
@@ -9,7 +9,7 @@ def generate_worksheet(output_path):
     Build the PDF document
     """
 
-    doc = Document(f'{output_path}/doc', geometry_options=GEOMETRY_OPTIONS)
+    doc = Document(output_path, geometry_options=GEOMETRY_OPTIONS)
 
     with doc.create(Section('The simple stuff')):
         doc.append('Some regular text and some')
@@ -19,13 +19,13 @@ def generate_worksheet(output_path):
         with doc.create(Subsection('Math that is incorrect')):
             doc.append(Math(data=['2*3', '=', 9]))
     
-    doc.generate_pdf(f'{output_path}/full', clean_tex=False)
+    doc.generate_pdf(output_path, clean_tex=False)
 
 
 if __name__ == "__main__":
     description = """A python program to compile LaTex documents"""
     parser = ArgumentParser(description=description)
-    parser.add_argument('OUTPUT_DIR', help="The dircotry in which to store the compiled PDF")
+    parser.add_argument('OUTPUT_PATH', help="The dircotry in which to store the compiled PDF")
     args = parser.parse_args()
 
-    generate_worksheet(args.OUTPUT_DIR)
+    generate_worksheet(args.OUTPUT_PATH)
